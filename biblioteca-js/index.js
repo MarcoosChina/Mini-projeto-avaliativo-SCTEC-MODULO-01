35c1f2135148b8ad86e761c7d4a10a673e2f7600
Marcos Vinicius Baumgartner <94867160+MarcoosChina@users.noreply.github.com>
2026-08-10 20:13:38 -0300

    adicionando o MENU, e finalizando a branch marcos

--- biblioteca-js/index.js
+++ biblioteca-js/index.js
@@ -17,21 +17,36 @@ import { listarDisponiveis } from './listarDisponiveis.js';
 import { listarLivros } from './listarLivros.js';
 import { cadastrarLivro } from './cadastrarLivro.js';
 import PromptSync from 'prompt-sync';
-
 const prompt = PromptSync();
 
-const opcao = prompt('Digite uma opcao: ');
+let opcao = '';
+do{
+    console.log('================================================');
+    console.log('SISTEMA DE BIBLIOTECA autores: Marcos e Gregory');
+    console.log('================================================');
+    console.log('1- LISTAR LIVROS');
+    console.log('2- BUSCAR LIVRO');
+    console.log('3- LISTAR LIVROS DISPONIVEIS');
+    console.log('4- CADASTRAR LIVRO');
+    console.log('5- REALIZAR EMPRÉSTIMO');
+    console.log('6- REALIZAR DEVOLUÇÃO');
+    console.log('7- EXIBIR ESTATISTICAS');
+    console.log('0- SAIR');
+    console.log('================================================');
+    opcao = prompt('Digite uma opcao: ');
 
-if (opcao === '1'){
-    listarLivros();
-}
-if (opcao === '2'){
-    const titulo = prompt('Digite o título do livro que você quer buscar: ');
-    buscarLivro(titulo);
-}
-if (opcao === '3'){
-    listarDisponiveis();
-}
-if (opcao === '4'){
-    cadastrarLivro();
-}
+    if (opcao === '1'){
+        listarLivros();
+    }
+    if (opcao === '2'){
+        const titulo = prompt('Digite o título do livro que você quer buscar: ');
+        buscarLivro(titulo);
+    }
+    if (opcao === '3'){
+        listarDisponiveis();
+    }
+    if (opcao === '4'){
+        cadastrarLivro();
+    }
+}while (opcao != '0');
+console.log('Saindo do sistema...');
